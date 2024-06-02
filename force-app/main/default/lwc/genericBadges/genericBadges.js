0186c65e28c4d561a9843fe9bce59bbdf8fd7838
Fernando Fangosi <fernando.fangosi@gft.com>
2024-06-02 02:29:05 -0300

    #GenericBadges Ajustes para leitura do campo e criação dos badges

--- force-app/main/default/lwc/genericBadges/genericBadges.js
+++ force-app/main/default/lwc/genericBadges/genericBadges.js
@@ -1,7 +1,73 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { getPicklistValuesByRecordType } from "lightning/uiObjectInfoApi";
 
 export default class GenericBadges extends LightningElement {
     // PUBLIC VARS
     @api fieldName;
+    @api iconName;
+    @api recordId;
 
+    // PRIVATE VARS
+    arrBadges = [];
+    objectName;
+    rtId;
+
+    // WIRES
+    @wire(getRecord, { recordId: '$recordId', fields: '$fieldName' })
+    getRecordInfo({data,error}){
+        try{
+            if(data && data.fields){
+                console.log(data);
+
+                this.objectName = data['apiName'];
+                this.rtId = data['recordTypeId'];
+
+                let badgeValues = data.fields[this.fieldName.split('.')[1]].value;
+
+                console.log(badgeValues);
+                console.log(this.objectName);
+                console.log(this.rtId);
+
+                if(badgeValues){
+                    this.arrBadges = badgeValues.split(';');
+                }
+
+                // this.forceDisableBtn = (this.redirectValue && RegExp(/^[a-zA-Z0-9]+$/).test(this.redirectValue) && this.redirectValue.length >= 15 && this.redirectValue.length <= 18) ? false : true;
+            }else{
+                // this.forceDisableBtn = true;
+            }
+        }catch(e){
+            // this.forceDisableBtn = true;
+        }
+    }
+
+    // @wire(getPicklistValuesByRecordType, { objectApiName: '$objectName', recordTypeId: "$rtId" })
+    // getFieldValues({data,error}){
+    //     try{
+    //         console.log('getPicklistValuesByRecordType'+data);
+    //         // if(data && data.fields){
+    //         //     console.log(data);
+
+    //         //     this.badgeValues = data.fields[this.relatedRecord.split('.')[1]].value;
+    //         //     this.objectName = data.fields[this.relatedRecord.split('.')[0]];
+    //         //     this.rtId = data['recordTypeId'];
+
+    //         //     // if(this.badgeValues && this.badgeValues.includes(';')){
+                    
+    //         //     // }
+
+    //         //     // this.forceDisableBtn = (this.redirectValue && RegExp(/^[a-zA-Z0-9]+$/).test(this.redirectValue) && this.redirectValue.length >= 15 && this.redirectValue.length <= 18) ? false : true;
+    //         // }else{
+    //         //     // this.forceDisableBtn = true;
+    //         // }
+    //     }catch(e){
+    //         console.log(e);
+    //     }
+    // }
+
+    // PRIVATE METHODS
+    teste(){
+        console.log('testeclick');
+    }
 }
